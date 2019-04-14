71a5ee5cd2112ba9666255d994cee59ee9c0dba5
Raghu Reddy <raghureddy.ram@gmail.com>
2019-04-14 15:26:10 -0700

    Add results component

---
+++ app/components/Results.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class Results extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (<div>Results</div>)
+    }
+}
+
+module.exports = Results;
